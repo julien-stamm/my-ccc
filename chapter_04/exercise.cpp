45178836d534c4e6b308980082d0e7b1d4276ca8
Julien Stamm <5460657+julien-stamm@users.noreply.github.com>
2024-03-15 22:24:38 +0000

    Finished Chapter 4 exercises

--- chapter_04/exercise.cpp
+++ chapter_04/exercise.cpp
@@ -1,20 +1,34 @@
-#include <ctime>
 #include <cstdio>
 #include <iostream>
+#include <ctime>
+#include <stdexcept>
+#include <utility>
 
 struct TimerClass {
-    TimerClass() :
-        timestamp{ std::clock() } {
+    TimerClass(const char* name) :
+        timestamp{ std::clock() },
+        name{ name } {
     }
     ~TimerClass() {
         if (timestamp > 0) {
-            std::cout << "Age of the timer: " << (std::clock() - timestamp) << std::endl;
-        }
+            std::cout << "Age of the timer \"" << name << "\": " << timer_age() << std::endl;
+        } 
     }
 
     // Copy constructor
-    TimerClass(const TimerClass& other) :
-        timestamp{ other.timestamp } {}
+    TimerClass(const char* name, const TimerClass& other) :
+        name{ name },
+        timestamp{ other.timestamp } {
+            int a = 0;
+        }
+
+    // Move constructor
+    TimerClass(TimerClass&& other) noexcept :
+        name{ other.name },
+        timestamp{ other.timestamp } {
+            other.name = nullptr;
+            other.timestamp = 0;
+        }
 
     // Copy assignment
     TimerClass& operator=(const TimerClass& other) {
@@ -23,41 +37,54 @@ struct TimerClass {
         return *this;
     }
 
-    // Move constructor
-    TimerClass(TimerClass&& other) noexcept :
-        timestamp{ other.timestamp } {
-            other.timestamp = 0;
-        }
-
     // Move assignment
     TimerClass& operator=(TimerClass&& other) noexcept {
         if (this == &other) return *this;
+        name = other.name;
         timestamp = other.timestamp;
+        other.name = nullptr;
         other.timestamp = 0;
         return *this;
     }
 private:
     std::clock_t timestamp;
+    const char* name;
+
+    const int timer_age() {
+        return std::clock() - timestamp;
+    }
 };
 
 int main() {
-    TimerClass a{};
-    TimerClass b{ a }; // Call copy constructor
+    TimerClass a{ "a" };
+    TimerClass b{ "b", a }; // Call copy constructor
     
-    TimerClass c{};
-    TimerClass d{};
-    d = c; // Call copy assignment
+    TimerClass c{ "c" };
+    TimerClass d{ std::move(c) }; // Call move constructor
+    // c is in a move-from state
 
-    TimerClass e{};
-    TimerClass f{ std::move(e) }; // Call move constructor
+    TimerClass e{ "e" };
+    TimerClass f{ "f" };
+    f = e; // Call copy assignment
 
-    TimerClass g{};
-    TimerClass h{};
+    TimerClass g{ "g" };
+    TimerClass h{ "h" };
     h = std::move(g); // Call move assignment
+    // g is in a move-from state
 
-    // e and g destructors don't print messages
+    // d and h will print as 'c' and 'g' on destruction
 
-    // TODO: Do 4-5 next
+    {
+        TimerClass math_timer{ "Math Timer" };
+        std::cout << "-- Calculation starting --\n";
+        for (auto i = 0; i < 1000000000; i++) {
+            if (i % 100000000 == 0) {
+                std::cout << "|";
+            }
+        }
+        std::cout << "\n-- Calculation finished --\n";
+    }
 
+    std::cout << "\n-- End of main --\n";
     return 0;
 }
